3e3e5a0f137103d359c089e4877aa2b134e8f3b4
DAMA-AND-HAT\LG <yungyeongjo.0918@gmail.com>
2026-05-07 21:10:47 +0900

    ss

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Todo } from '../App';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://todolist-qk2k.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
