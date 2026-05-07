69565020c61543deeb46afa62695338b1ff12711
DAMA-AND-HAT\LG <yungyeongjo.0918@gmail.com>
2026-05-07 21:35:17 +0900

    fa

--- backend/src/routes/todoRoutes.js
+++ backend/src/routes/todoRoutes.js
@@ -4,15 +4,15 @@ const { getTodos, createTodo, updateTodo, deleteTodo } = require('../controllers
 const router = express.Router();
 
 // GET /todos - 전체 할 일 조회
-router.get('/', getTodos);
+router.get('/todos', getTodos);
 
 // POST /todos - 할 일 추가
-router.post('/', createTodo);
+router.post('/todos', createTodo);
 
 // PATCH /todos/:id - 완료 상태 토글
-router.patch('/:id', updateTodo);
+router.patch('/todos/:id', updateTodo);
 
 // DELETE /todos/:id - 할 일 삭제
-router.delete('/:id', deleteTodo);
+router.delete('/todos/:id', deleteTodo);
 
 module.exports = router;
